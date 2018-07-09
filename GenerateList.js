32d35cc5dee4970ce933120c9934f506ac0bfb70
Jerry Zhang <jerry.w.zhang@gmail.com>
2018-07-09 19:46:36 -0400

    Edit errors, put Probabilities.txt file on internet

--- GenerateList.js
+++ GenerateList.js
@@ -1,5 +1,5 @@
 // Author: Jerry Zhang
-var file = 'C:/Users/lingchen/AutismProject/Probabilities.txt'
+var file = 'https://gist.githubusercontent.com/thejwzhang/af7517ff59667192288320db2205f6f8/raw/50e29433823b3746a5120e382a8604ef4d764189/Probabilities.txt'
 
 var NUM_TRIES = 200;
 var NUM_CMD = 4;
@@ -7,13 +7,12 @@ var NUM_POSSIBLE_OUTCOMES = 3;
 
 function readFile(callback) {
   var txtFile = new XMLHttpRequest();
-  txtFile.open("GET", "file://" + file, true);
+  txtFile.open("GET", file, true);
   txtFile.onreadystatechange = function() {
     if (txtFile.readyState === 4) {  // Ready to parse
       if (txtFile.status === 200) {  // File found
         allText = txtFile.responseText;
         lines = allText.split("\n"); // Separates each line into an array
-        // console.log(lines);
         callback(lines);
       }
     }
@@ -21,7 +20,9 @@ function readFile(callback) {
   txtFile.send(null);
 }
 
-readFile(function(linesFromFile) { // callback checks to make sure the file is valid and converts string to numbers
+var finalList = [];
+
+readFile(function(linesFromFile) { // callback function checks to make sure the file is valid and converts string to numbers
   var lines = []; // This is a brand new array to hold the valid lines from the txt file in number format
   // console.log(linesFromFile);
   for (var i in linesFromFile) {
@@ -30,7 +31,7 @@ readFile(function(linesFromFile) { // callback checks to make sure the file is v
       var splitNums = currentLine.split(" ");
       if (splitNums.length - 1 != NUM_POSSIBLE_OUTCOMES - 1) { // Make sure there are 2 spaces per line (3 items)
         console.log("ERROR:  Incorrect number of items");
-        alert("ERROR: Incorrect number of items");
+        alert("ERROR: Incorrect number of items!. Check the probabilities.txt file!");
       } else {
         // console.log(splitNums);
         var tempAdd = 0;
@@ -41,13 +42,15 @@ readFile(function(linesFromFile) { // callback checks to make sure the file is v
             if (tempAdd != 1) {
               var lineNum = Number(i)+1;
               console.log("ERROR: Probability for line " +  lineNum + " doesn't add up to 1. It adds up to " + tempAdd + ".");
-              alert("ERROR: Probability for line " + lineNum + " doesn't add up to 1. It adds up to " + tempAdd + ".");
+              alert("ERROR: Probability for line " + lineNum + " doesn't add up to 1. It adds up to " + tempAdd + ". Check the probabilities.txt file!");
             }
           }
-
           if (!isNaN(a)) {
             lines.push(a);
-          } // end if (!isNaN(a))
+          } else {
+            console.log("ERROR: One or more of the items in the file is not a number.");
+            alert("ERROR: One or more of the items in the file is not a number. Check the probabilities.txt file!");
+          } // end else: if (!isNaN(a))
         } // end for (var num in splitNums)
       } // end else: if (splitNums.length - 1 != NUM_POSSIBLE_OUTCOMES - 1)
     } // end if (currentLine)
@@ -55,6 +58,8 @@ readFile(function(linesFromFile) { // callback checks to make sure the file is v
 
   if (lines.length != NUM_CMD*NUM_POSSIBLE_OUTCOMES) {
     console.log("ERROR: Not enough lines");
+    alert("ERROR: Not enough lines! Check the probabilities.txt file!")
   }
-  console.log(lines);
+  finalList = lines;
+  console.log(finalList);
 });
