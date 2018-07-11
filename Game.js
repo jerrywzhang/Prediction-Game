929a406a0d7d8f49c18627a99b14f51269d4fd9d
Jerry Zhang <jerry.w.zhang@gmail.com>
2018-07-11 15:50:41 -0400

    Testing

--- Game.js
+++ Game.js
@@ -29,25 +29,37 @@ function getResult(done) { // called as the callback of createOutcomes
 
 var timePressed = 0;
 var keyPressed = 0;
+var lastKeyPressed = 0;
+
+document.addEventListener("keydown", function(event) {
+  var key = event.keyCode;
+  if (lastKeyPressed == key) {
+    keyPressed = 0;
+    console.log("HELD DOWN " + keyPressed);
+  } else if (key == 49 || key == 97) {
+    timePressed = performance.now();
+    console.log("1 Pressed");
+    keyPressed = 1;
+  } else if (key == 50 || key == 98) {
+    timePressed = performance.now();
+    console.log("2 Pressed");
+    keyPressed = 2;
+  } else if (key == 51 || key == 99) {
+    timePressed = performance.now();
+    console.log("3 Pressed");
+    keyPressed = 3;
+  } else if (key == 13) {
+    // console.log("enter");
+  } else {
+    // console.log("Key Number " + key + " Pressed");
+    keyPressed = 0;
+  }
+  lastKeyPressed = key;
+  // console.log(timePressed);
+});
 
 document.addEventListener("keyup", function(event) {
-    timePressed = performance.now();
-    if (event.keyCode == 49 || event.keyCode == 97) {
-      console.log("1 Pressed");
-      keyPressed = 1;
-
-    } else if (event.keyCode == 50 || event.keyCode == 98) {
-      console.log("2 Pressed");
-      keyPressed = 2;
-    } else if (event.keyCode == 51 || event.keyCode == 99) {
-      console.log("3 Pressed");
-      keyPressed = 3;
-    } else if (event.keyCode == 13) {
-      console.log("enter");
-    } else {
-      console.log("Key Number " + event.keyCode + " Pressed");
-      keyPressed = 0;
-    }
+  lastKeyPressed = 0;
 });
 
 var animate = window.requestAnimationFrame ||
@@ -116,31 +128,91 @@ Ball.prototype.render = function() {
 
 var player = new Player();
 var ball = new Ball(WINDOW_WIDTH/2, 25, "#0000FF");
-var ball2 = new Ball(WINDOW_WIDTH/2 - 20, 25, "#FF0000");
+// var ball2 = new Ball(WINDOW_WIDTH/2 - 20, 25, "#FF0000");
 
 var render = function() {
   context.fillStyle = "#FFFFFF"; // hex color: white
   context.fillRect(0, 0, width, height);
   player.render();
   ball.render();
-  ball2.render();
+  // ball2.render();
 };
 
+var arrayCounter = 0;
+var hitCounter = 0;
+var missCounter = 0;
+var doneOnce = false;
+
 Ball.prototype.update = function(basket) {
-  if (this.y <= WINDOW_HEIGHT/2) {
+  // console.log(arrayCounter + " " + outcomesArray[arrayCounter]);
+  if (this.y <= WINDOW_HEIGHT*3/4) {
+    this.x_speed = 0;
+    this.y_speed = BASE_SPEED_Y;
     this.x += this.x_speed;
     this.y += this.y_speed;
+    console.log(outcomesArray[arrayCounter] + " " + doneOnce + " " + keyPressed);
+    if (keyPressed == outcomesArray[arrayCounter] && !doneOnce) {
+      hitCounter++;
+      keyPressed = 0;
+      console.log("HIT");
+      // console.log(performance.now() - timePressed);
+      doneOnce = true;
+      document.getElementById("hit").innerHTML = "HIT";
+    } else if (keyPressed != 0) {
+      console.log("MISS");
+      keyPressed = 0;
+      missCounter++;
+      doneOnce = true;
+      document.getElementById("hit").innerHTML = "MISS";
+    }
   } else {
-    // console.log("DOWN!");
+    console.log("TOO LATE!!!!!!!!!!!!!");
+    doneOnce = false;
+    if (outcomesArray[arrayCounter] == 1) {
+      this.x_speed = -30;
+      this.y_speed = 16;
+    } else if (outcomesArray[arrayCounter] == 2) {
+      this.x_speed = 0;
+      this.y_speed = 16;
+    } else if (outcomesArray[arrayCounter] == 3) {
+      this.x_speed = 30;
+      this.y_speed = 16;
+    } else if (outcomesArray[arrayCounter] == 999) {
+      this.x_speed = 0;
+      this.y_speed = 0;
+    } else {
+      alert("There's an issue!");
+    }
+    this.x += this.x_speed;
+    this.y += this.y_speed;
+    if (this.y > WINDOW_HEIGHT - 20) {
+      this.x = WINDOW_WIDTH/2;
+      this.y = 25;
+      keyPressed = 0;
+      if (arrayCounter < 199) {
+        arrayCounter++;
+        // sleep(100);
+      } else {
+        alert("You've finished the game!");
+        outcomesArray.push(999);
+        arrayCounter = 200;
+        this.x = WINDOW_WIDTH/2;
+        this.y = 25;
+        console.log("HIT: " + hitCounter + " MISS: " + missCounter + " %: " + hitCounter/200);
+        console.log(hitCounter + missCounter);
+      }
+    }
+    // console.log(performance.now());
   }
 };
 
 Player.prototype.update = function() {
   // console.log("KP: " + keyPressed + " " + timePressed);
   var timeDifference = performance.now() - timePressed;
-  console.log(timeDifference);
-  if (timeDifference > 2000) {
+  // console.log(performance.now());
+  if (timeDifference > 200) {
     this.basket.move(888, 888); // off the screen
+    keyPressed = 0;
   } else if (keyPressed == 1) {
     this.basket.move(WINDOW_WIDTH/2 - 200, WINDOW_HEIGHT-20);
   } else if (keyPressed == 2) {
@@ -148,7 +220,6 @@ Player.prototype.update = function() {
   } else if (keyPressed == 3) {
     this.basket.move(WINDOW_WIDTH/2 + 175, WINDOW_HEIGHT-20);
   }
-  keyPressed = 0;
 };
 
 Basket.prototype.move = function(x, y) {
@@ -159,11 +230,14 @@ Basket.prototype.move = function(x, y) {
 var update = function() {
   player.update();
   ball.update(player.basket);
+  // ball2.update(player.basket);
 };
 
-// KEY CODES: https://www.cambiaresearch.com/articles/15/javascript-char-codes-key-codes
-// 1: 49, numpad 97
-// 2: 50, numpad 98
-// 3: 51, numpad 99
-// Left arrow: 37
-// right arrow: 39
+function sleep(milliseconds) {
+  var start = performance.now();
+  for (var i = 0; i < 1e7; i++) {
+    if ((performance.now() - start) > milliseconds){
+      break;
+    }
+  }
+}
