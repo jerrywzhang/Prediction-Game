acc9aa4d05c78f81cc165f11c8a3b523ad204e9b
Jerry Zhang <jerry.w.zhang@gmail.com>
2018-07-20 18:23:01 -0400

    Edit box position

--- Temporal/Game.js
+++ Temporal/Game.js
@@ -309,10 +309,10 @@ Player.prototype.update = function() {
   } else if (keyPressed == 1) {
     this.basket.move(WINDOW_WIDTH/2 - 25, WINDOW_HEIGHT-100, 0);
   } else if (keyPressed == 2) { // rotation matrix calculator used: http://www.wolframalpha.com/widgets/view.jsp?id=bd71841fce4a834c804930bd48e7b6cf
-    this.basket.move(9.81+25, 237-50, -30); // calculated from rotation matrix for 30 degrees counterclockwise. xy coordinate in normal plane: (300-(100)*sqrt(3), 200)
-                                              // then translated 25 left (+25), 50 up (-50).
+    this.basket.move(9.81-25, 237-50, -30); // calculated from rotation matrix for 30 degrees counterclockwise. xy coordinate in normal plane: (300-(100)*sqrt(3), 200)
+                                            // then translated 25 left (+25), 50 up (-50).
   } else if (keyPressed == 3) {
-    this.basket.move(484.808, -63.3975, 30); // calculated from rotation matrix for 30 degrees clockwise. xy coordinate in normal plane: (300+(100-12.5)*sqrt(3), 200-12.5)
+    this.basket.move(510-25, -63.4-50, 30); // calculated from rotation matrix for 30 degrees clockwise. xy coordinate in normal plane: (300+(100-12.5)*sqrt(3), 200-12.5)
   }
 };
 
